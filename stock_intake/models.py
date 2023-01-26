8847552183eda07c1ff04d7250e0ba11a938078a
jogorska <gorska919@gmail.com>
2023-01-26 23:49:29 +0000

    Update models in stock_intake app

--- stock_intake/models.py
+++ stock_intake/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class PurchaseInvoice(models.Model):
-    invoice_number = models.CharField()
-    date_issued = models.CharField()
-    total_net = models.CharField()
-    VAT_percentage = models.IntegerField()
-    suplier_name = models.CharField()
-    delivery_date = models.DateField()
-    invoice_due_date = models.DateField()
-    paid = models.BooleanField()
+    invoice_number = models.CharField(max_length=220, unique=True, blank=False, null=False)
+    date_issued = models.DateField(blank=False, null=False)
+    total_net = models.DecimalField(decimal_places=2, max_digits=20, blank=False, null=False)
+    VAT_percentage = models.IntegerField(max_length=2)
+    suplier_name = models.CharField(max_length=220, unique=True, blank=False, null=False)
+    delivery_date = models.DateField(blank=False, null=False)
+    invoice_due_date = models.DateField(blank=True, null=True)
+    paid = models.BooleanField(blank=False, null=False)
 
-# todo supplier model
+    def __str__(self):
+        return self.invoice_number
