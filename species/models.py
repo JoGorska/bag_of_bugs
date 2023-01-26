34b519f86c0442a29cc40c67c1dfd8e2e60762b5
jogorska <gorska919@gmail.com>
2023-01-26 23:45:33 +0000

    Update models in species app

--- species/models.py
+++ species/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
 
-# Create your models here.
 class Species(models.Model):
-    name = models.CharField()
-    latin_name = models.CharField()
-    description = models.CharField()
-    dangerous_species = models.BooleanField()
+    name = models.CharField(max_length=220, unique=True, blank=False, null=False)
+    latin_name = models.CharField(max_length=220, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    dangerous_species = models.BooleanField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=220, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Size(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=220, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class EnviromentRequirements(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=220, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
