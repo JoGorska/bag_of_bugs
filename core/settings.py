a95d9f6abb5809a05b834760def76520c47b14ab
jogorska <gorska919@gmail.com>
2023-01-31 22:10:41 +0000

    Add csrf trusted origin settings for django admin to work and add database setting allowing run sqlite locally

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,7 @@ if DEVELOPMENT:
 else:
     ALLOWED_HOSTS = ['127.0.0.1', 'bagofbugs-production.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = ['https://bagofbugs-production.up.railway.app']
 
 # Application definition
 
@@ -97,18 +98,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+if 'DATABASE_URL' in os.environ:
 
-DATABASES = {
-        'default': dj_database_url.parse(env("DATABASE_URL"))
+    DATABASES = {
+            'default': dj_database_url.parse(env("DATABASE_URL"))
+        }
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
