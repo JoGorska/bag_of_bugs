f1a559823afb45f61e6c97777766f37c19949142
jogorska <gorska919@gmail.com>
2023-02-03 22:56:02 +0000

    Add fixtures for species

---
+++ core/tests/fixtures.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+import pytest
+
+
+@pytest.fixture
+def user_one(client):
+    return User.objects.get_or_create(
+        email='user.one@test.me',
+        password='supersecret',
+        username='user',
+        first_name='User',
+        last_name='One'
+    )[0]
---
+++ species/tests/fixtures.py
@@ -0,0 +1,39 @@
+import pytest
+from ..models import Species, Size, Category, Enviroment
+
+
+
+@pytest.fixture
+def category():
+    return Category.objects.get_or_create(
+        name='Spider'
+    )[0]
+
+
+@pytest.fixture
+def size():
+    return Size.objects.get_or_create(
+        name='Small'
+    )[0]
+
+
+@pytest.fixture
+def enviroment():
+    return Enviroment.objects.get_or_create(
+        name='Indoor'
+    )[0]
+
+
+@pytest.fixture
+def species(category, size, enviroment):
+    return Species.objects.get_or_create(
+        name='Harry Larry',
+        price=20.11,
+        latin_name='Spiderus Wlochatus',
+        description='very long and descriptive \
+            comment about habits of this dangerous spider',
+        dangerous_species=True,
+        category=category,
+        size=size,
+        enviroment=enviroment
+    )[0]
